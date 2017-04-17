faf13ef947fc0de3d89a19df2047f11e5ae938f1
akoira <akoira>
2017-04-17 15:25:48 +0545

    Added hotel marker

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([27.68087,85.30660], 15);
+var mymap = L.map('mapid').setView([27.68065,85.30619], 15);
 
 var CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
@@ -11,6 +11,6 @@ var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 })
 
-// var marker = L.marker([27.68087,85.30660]).addTo(mymap);
+var marker = L.marker([27.68065,85.30619]).addTo(mymap);
 
 CartoDB_Positron.addTo(mymap);
