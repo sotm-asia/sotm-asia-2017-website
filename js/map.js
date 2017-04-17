7db4a62824ffc16e9c6f0d26536f03498fb9f5a7
akoira <akoira>
2017-04-17 15:03:07 +0545

    Changed to green theme

---
+++ js/map.js
@@ -0,0 +1,16 @@
+var mymap = L.map('mapid').setView([27.68087,85.30660], 15);
+
+var CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+
+var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+
+// var marker = L.marker([27.68087,85.30660]).addTo(mymap);
+
+CartoDB_Positron.addTo(mymap);
